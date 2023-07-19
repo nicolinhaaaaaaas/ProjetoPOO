9687509a5333774a7fd2af05df04474fa631c9db
Nicolas <112559599+nicolinhaaaaaaas@users.noreply.github.com>
2023-07-19 16:56:53 -0300

    Update Hotel.java

--- Hotel.java
+++ Hotel.java
@@ -3,9 +3,58 @@ import java.util.*;
 
 public class Hotel {
 
-	ArrayList<String> hotelNomes = new ArrayList<>();
+	private String nomeHotel;
+	private String enderecoHotel;
+	private String descricaoHotel;
+	private Integer quartoHotel;
 	
-	void adicionar() {
+	
+	Hotel(String nomeHotel, String enderecoHotel, String descricaoHotel, Integer quartoHotel){
+		this.nomeHotel = nomeHotel;
+		this.enderecoHotel = enderecoHotel;
+		this.descricaoHotel = descricaoHotel;
+		this.quartoHotel = quartoHotel;
+	}
+	
+	public void setNomeHotel(String nomeHotel) {
+		this.nomeHotel = nomeHotel;
+	}
+	public void setEndereco(String enderecoHotel) {
+		this.enderecoHotel = enderecoHotel;
+	}
+	public void setDescricao(String descricaoHotel) {
+		this.descricaoHotel = descricaoHotel;
+	}
+	public void setQuarto(Integer quartoHotel) {
+		this.quartoHotel = quartoHotel;
+	}
+	public String getNomeHotel() {
+		return nomeHotel;
+	}
+	public String getEndereco() {
+		return enderecoHotel;
+	}
+	public String getDescricao() {
+		return descricaoHotel;
+	}
+	public Integer getQuarto() {
+		return quartoHotel;
+	}
+	
+	public void adicionarHotel() {
 		
+		
+	}
+	
+	public void removerHotel() {
+		
+	}
+	
+	public void modificarHotel() {
+		
+	}
+	
+	public String toString() {
+		return nomeHotel + enderecoHotel + descricaoHotel + quartoHotel;
 	}
 }

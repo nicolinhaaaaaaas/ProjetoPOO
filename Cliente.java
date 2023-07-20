34576e12f692ac1e730723f6b3f622fb446fcfc5
Nicolas <112559599+nicolinhaaaaaaas@users.noreply.github.com>
2023-07-20 08:59:23 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,21 +1,42 @@
 package projeto;
 
-public class Cliente {
+import java.util.Scanner;
 
+public class Cliente extends Hotel{
 	
+	Scanner scanner = new Scanner(System.in);
+
+	Cliente(String nomeHotel, String enderecoHotel, String descricaoHotel, Integer quartoHotel, Boolean hotelExiste, String nomeCliente) {
+		super(nomeHotel, enderecoHotel, descricaoHotel, quartoHotel, hotelExiste);
+		this.nomeCliente = nomeCliente;
+	}
+
+	private String nomeCliente;
 	
-	void verOpcao() {
-		
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+	public String getNomeCliente() {
+		return nomeCliente;
 	}
 	
-	void alugarQuarto() {
+	public void escolherHotel() {
+		// so pra testar
+		System.out.println(super.getQuarto());
 		
+		System.out.print("Quantos quartos deseja alugar? ");
+		int alugarQuartos = scanner.nextInt();
+		
+		int quartosRestantes = super.getQuarto() - alugarQuartos;
+		
+		super.setQuarto(quartosRestantes);
+		
+		System.out.println("Aluguel bem sucedido!");
+		
+		// pra testar
+		System.out.println(super.getQuarto());
 	}
 	
-	void irEmbora() {
-		
-	}
-	
-	
+	// public void avaliacao(){
 	
 }

b72eea668a411b3df2d94ecf171c7ca7c56db541
Nicolas <112559599+nicolinhaaaaaaas@users.noreply.github.com>
2023-07-27 09:20:55 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,20 @@
+package prototipoPOO;
+
+public class Cliente {
+	
+	private String nomeCliente;
+
+	
+	
+	
+	
+	
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+}

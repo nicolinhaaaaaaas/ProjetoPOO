5e4e28ff1ba072a92da3aaa7fc96f93f6de5ac8a
Nicolas <112559599+nicolinhaaaaaaas@users.noreply.github.com>
2023-07-27 09:20:07 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,59 @@
+package prototipoPOO;
+
+import java.util.*;
+
+public class Menu {
+	
+	static void cadastrarHotel() {
+		
+		String nomeHotel;
+		String enderecoHotel;
+		String descricaoHotel;
+		int quartosHotel;
+		
+		
+		
+	}
+	
+	static void editarHotel() {
+		
+	}
+	
+	static void removerHotel() {
+		
+	}
+	
+	static void listarHotel() {
+		
+	}
+	
+	static void cadastrarCliente() {
+		
+	}
+	
+	static void hoteisDisponiveis() {
+		
+	}
+	
+	static void alugarQuarto() {
+		
+	}
+	
+	static void irEmboraQuarto() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	static void clearBuffer(Scanner scanner) {
+        if (scanner.hasNextLine()) {
+            scanner.nextLine();
+        }
+    }
+
+}

2b5a9050cae477ec7569d73c656801bbed98a317
Nicolas <112559599+nicolinhaaaaaaas@users.noreply.github.com>
2023-07-27 09:21:25 -0300

    Create Hotel.java

---
+++ Hotel.java
@@ -0,0 +1,57 @@
+package prototipoPOO;
+
+public class Hotel {
+	
+	private String nomeHotel;
+	private String enderecoHotel;
+	private String descricaoHotel;
+	private int quartosDisnponiveisHotel;
+	private int quartosAlugadosHotel;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public String getNomeHotel() {
+		return nomeHotel;
+	}
+	public void setNomeHotel(String nomeHotel) {
+		this.nomeHotel = nomeHotel;
+	}
+	public String getEnderecoHotel() {
+		return enderecoHotel;
+	}
+	public void setEnderecoHotel(String enderecoHotel) {
+		this.enderecoHotel = enderecoHotel;
+	}
+	public String getDescricaoHotel() {
+		return descricaoHotel;
+	}
+	public void setDescricaoHotel(String descricaoHotel) {
+		this.descricaoHotel = descricaoHotel;
+	}
+	public int getQuartosDisnponiveisHotel() {
+		return quartosDisnponiveisHotel;
+	}
+	public void setQuartosDisnponiveisHotel(int quartosDisnponiveisHotel) {
+		this.quartosDisnponiveisHotel = quartosDisnponiveisHotel;
+	}
+	public int getQuartosAlugadosHotel() {
+		return quartosAlugadosHotel;
+	}
+	public void setQuartosAlugadosHotel(int quartosAlugadosHotel) {
+		this.quartosAlugadosHotel = quartosAlugadosHotel;
+	}
+	
+	
+	
+
+}
